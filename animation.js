c6b56968948f93a21a3110b62d73273353bb7b6a
Santiago Suarez <santidev@Santiagos-MacBook-Pro-2.local>
2020-12-22 17:44:01 -0600

    initial commit for popmotion

---
+++ animation.js
@@ -0,0 +1,22 @@
+const popmotion = require("popmotion");
+
+const {styler, spring, listen, pointer, value} = popmotion;
+// code taken from one of the examples on popmotion.io
+const ball = document.querySelector(".box");
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, "mousedown touchstart").start(e => {
+  e.preventDefault();
+  pointer(ballXY.get()).start(ballXY);
+});
+
+listen(document, "mouseup").start(() => {
+    spring({
+      from: ballXY.get(),
+      velocity: ballXY.getVelocity(),
+      to: { x: 0, y: 0 },
+      stiffness: 200
+    })
+    .start(ballXY);
+});
